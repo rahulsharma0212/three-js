687966934f5ad9a90b9021df4f2a2267800d7c47
Rahul Sharma <rahulsharma02121998@gmail.com>
2024-04-28 07:42:09 +0530

    animation done

--- 06-Animations/main.js
+++ 06-Animations/main.js
@@ -1,3 +1,4 @@
+import gsap from "gsap";
 import "./style.css";
 import * as THREE from "three";
 
@@ -27,4 +28,30 @@ const renderer = new THREE.WebGLRenderer({
     canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
-renderer.render(scene, camera);
+
+// let time = Date.now();
+const clock = new THREE.Clock();
+
+// gsap
+gsap.to(mesh.position, { x: 2, duration: 1, delay: 1 });
+gsap.to(mesh.position, { x: 0, duration: 1, delay: 2 });
+// Animations
+const tick = () => {
+    //delta
+    // const delta = Date.now() - time;
+    // time = Date.now();
+    // // update objects
+    // mesh.rotation.y += 0.001 * delta;
+
+    // const elapsedTime = clock.getElapsedTime();
+    // // mesh.rotation.y = elapsedTime;
+    // mesh.position.x = Math.sin(elapsedTime);
+    // mesh.position.y = Math.cos(elapsedTime);
+
+    // renderer
+    renderer.render(scene, camera);
+
+    window.requestAnimationFrame(tick);
+};
+
+tick();
