378b90633278e12ad04d9b4eabe75520932c60d5
Rahul Sharma <rahulsharma02121998@gmail.com>
2024-04-28 13:20:25 +0530

    geometries finished

--- 09-geometries/main.js
+++ 09-geometries/main.js
@@ -14,8 +14,23 @@ const scene = new THREE.Scene();
 /**
  * Object
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
+
+const geometry = new THREE.BufferGeometry();
+// const vertices = new Float32Array([0, 0, 0, 0, 1, 0, 0, 0, 1]);
+
+const count = 100;
+const vertices = new Float32Array(count * 3 * 3);
+for (let i = 0; i < count * 3 * 3; i++) {
+    vertices[i] = (Math.random() - 0.5) * 4;
+}
+
+geometry.setAttribute("position", new THREE.BufferAttribute(vertices, 3));
+
+const material = new THREE.MeshBasicMaterial({
+    color: 0xff0000,
+    wireframe: true,
+});
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
