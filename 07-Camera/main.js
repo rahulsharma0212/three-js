746fc3c7c0244d95d19b33ca42d7b2dbb9595c2e
Rahul Sharma <rahulsharma02121998@gmail.com>
2024-04-28 07:50:41 +0530

    camera init

---
+++ 07-Camera/main.js
@@ -0,0 +1,46 @@
+import gsap from "gsap";
+import "./style.css";
+import * as THREE from "three";
+
+// scene
+const scene = new THREE.Scene();
+
+// geometry
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({
+    color: 0xff0000,
+});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+const sizes = { width: 700, height: 400 };
+
+// camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.set(1, 1, 3);
+scene.add(camera);
+
+// renderer
+const canvas = document.createElement("canvas");
+document.body.appendChild(canvas);
+
+const renderer = new THREE.WebGLRenderer({
+    canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+
+// let time = Date.now();
+const clock = new THREE.Clock();
+
+// Animations
+const tick = () => {
+    const elapsedTime = clock.getElapsedTime();
+    mesh.rotation.y = elapsedTime;
+
+    // renderer
+    renderer.render(scene, camera);
+
+    window.requestAnimationFrame(tick);
+};
+
+tick();
